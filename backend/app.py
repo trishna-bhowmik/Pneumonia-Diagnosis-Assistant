b87bc8c298c1590b2ef8d679d10db89107cdc569
Trishna Bhowmik <thetrish474@gmail.com>
2025-11-08 19:29:58 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Medical Image Diagnosis API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
